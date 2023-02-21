f4fb3ff3b07b3b379e2944649eddb680ce0140e5
Viny-Pereira <vinypereira75@gmail.com>
2023-02-21 18:02:22 -0300

    add info of module operators

--- Sintaxe/java-sintaxe/src/DataTypes.java
+++ Sintaxe/java-sintaxe/src/DataTypes.java
@@ -6,11 +6,13 @@ public class DataTypes {
         long cpf = 630_000_000_000L; // use L to force the long type
         float PI = 3.14F; // F to force the float type
         double income = 1200.50;
-
+        System.out.println(age + year + cp + cpf + PI + income);
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; // casting
+        System.out.println(numeroCurto2);
         // constant
         final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
     }
 }
---
+++ Sintaxe/java-sintaxe/src/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+        String result = (a == b) ? "iguais" : "diferentes";
+        System.out.println(result);
+
+    }
+}
