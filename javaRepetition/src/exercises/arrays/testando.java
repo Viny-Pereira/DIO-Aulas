b99ec9e7ef52d46bf8eec05041b268f00d3655a6
Viny-Pereira <vinypereira75@gmail.com>
2023-03-03 15:57:08 -0300

    add springboot

---
+++ javaRepetition/src/exercises/arrays/testando.java
@@ -0,0 +1,4 @@
+public class testando {
+    public static void main(String[] args) {
+        String[][][][] vetor = {{{{"aguia"},{"pomba"}},{{"homem"},{"vaca"}}},{{{"pulga"},{"lagarta"}},{{"sanguessuga"},{"minhoca"}}}};
+}
